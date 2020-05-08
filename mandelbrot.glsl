45206fcb27fbea71192e81cfa05a0ecb99072aac
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-05-08 18:04:46 +0300

    Dopolnenije v šejdere množestva Mandeljbrota. Sdelan plavnyj gradient, dobavleno sglaživanije.

--- mandelbrot.glsl
+++ mandelbrot.glsl
@@ -1,25 +1,37 @@
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-	vec2 num= ( fragCoord.xy ) / ( 0.5 * iResolution.xy ) - vec2( 1.0, 1.0 );
-	num.x*= iResolution.x / iResolution.y;
-
-	const int c_iterations= 256;
-	vec2 z= vec2( 0.0, 0.0 );
-	int i= 0;
-	for(; i < c_iterations; ++i )
+	const int c_ss_size= 3;
+	fragColor= vec4( 0.0, 0.0, 0.0, 0.0 );
+	for( int dx= 0; dx < c_ss_size; ++dx )
+	for( int dy= 0; dy < c_ss_size; ++dy )
 	{
-		z= num + vec2( z.x * z.x - z.y * z.y, 2.0 * z.x * z.y );
-		if( dot(z, z) >= 4.0 )
-			break;
+		vec2 num= ( fragCoord.xy + vec2(dx, dy) / float(c_ss_size) ) / ( 0.5 * iResolution.xy ) - vec2( 1.0, 1.0 );
+		num.x*= iResolution.x / iResolution.y;
+
+		const int c_iterations= 128;
+		float c= 1.0;
+		if(dot(num, num) <= 4.0)
+		{
+			vec2 z= vec2( 0.0, 0.0 );
+			for(int i= 0; i < c_iterations; ++i )
+			{
+				z= num + vec2( z.x * z.x - z.y * z.y, 2.0 * z.x * z.y );
+				float d= dot(z, z);
+				if( d>= 4.0 )
+					break;
+
+				c+= 4.0 - d;
+			}
+		}
+
+		float color_factor= log(c) * 0.25;
+
+		vec3 color= vec3(
+			sin( iTime * 1.0 + color_factor * 7.0 ) * 0.5 + 0.5,
+			sin( iTime * 1.5 + color_factor * 8.0 ) * 0.5 + 0.5,
+			sin( iTime * 2.0 + color_factor * 9.0 ) * 0.5 + 0.5 );
+
+		fragColor+= vec4( color, 0.0 );
 	}
-
-	float set_factor= step( 4.0, dot(z, z) );
-	float color_factor= pow( float(i) / float(c_iterations), 0.25 );
-
-	vec3 color= vec3(
-		sin( iTime * 1.0 + color_factor * 7.0 ) * 0.5 + 0.5,
-		sin( iTime * 1.5 + color_factor * 8.0 ) * 0.5 + 0.5,
-		sin( iTime * 2.0 + color_factor * 9.0 ) * 0.5 + 0.5 );
-
-	fragColor= vec4( set_factor * color, 0.0 );
+	fragColor/= float(c_ss_size * c_ss_size);
 }
