06bb49dc10cef2bb6c6284127c70af3b3cd1dd6f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-11-18 11:05:10 +0300

    Dobavleny cörnyje tocki.

--- halftone_dots.glsl
+++ halftone_dots.glsl
@@ -10,23 +10,31 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 
 	float lod= log2( max( tex_size.x, tex_size.y ) / dot_scale );
 	vec4 tex_value= textureLod( iChannel0, tex_coord, lod );
-	vec3 cmy= vec3( 1.0 - tex_value.r, 1.0 - tex_value.g, 1.0 - tex_value.b );
+	tex_value.rgb= sqrt( tex_value.rgb );
+
+	float k= 1.0 - max( max( tex_value.r, tex_value.g ), tex_value.b );
+	k= min( k, 0.333 ); // Do not add too much black
+	vec3 cmy= vec3( 1.0 - tex_value.r - k, 1.0 - tex_value.g - k, 1.0 - tex_value.b - k );
 		
-	vec2 pattern_coord_r= fract( tex_coord * dot_scale + vec2( 0.0, 0.25 ) );
-	vec2 pattern_coord_g= fract( tex_coord * dot_scale + vec2( +sqrt(3.0) * 0.125, -0.125 ) );
-	vec2 pattern_coord_b= fract( tex_coord * dot_scale + vec2( -sqrt(3.0) * 0.125, -0.125 ) );
+	vec2 pattern_coord_r= fract( tex_coord * dot_scale + vec2( 0.0, 0.5 ) );
+	vec2 pattern_coord_g= fract( tex_coord * dot_scale + vec2( +sqrt(3.0) * 0.25, -0.25 ) );
+	vec2 pattern_coord_b= fract( tex_coord * dot_scale + vec2( -sqrt(3.0) * 0.25, -0.25 ) );
+	vec2 pattern_coord_k= fract( tex_coord * dot_scale );
 
 	vec2 v_r= pattern_coord_r.xy - vec2( 0.5, 0.5 );
 	vec2 v_g= pattern_coord_g.xy - vec2( 0.5, 0.5 );
 	vec2 v_b= pattern_coord_b.xy - vec2( 0.5, 0.5 );
+	vec2 v_k= pattern_coord_k.xy - vec2( 0.5, 0.5 );
 
 	float radius_r= sqrt(cmy.r / 2.0);
 	float radius_g= sqrt(cmy.g / 2.0);
 	float radius_b= sqrt(cmy.b / 2.0);
+	float radius_k= sqrt(k / 2.0);
 
 	float facror_r= smoothstep( max( radius_r - d, 0.0 ), min( radius_r + d, 1.0 ), length(v_r) );
 	float facror_g= smoothstep( max( radius_g - d, 0.0 ), min( radius_g + d, 1.0 ), length(v_g) );
 	float facror_b= smoothstep( max( radius_b - d, 0.0 ), min( radius_b + d, 1.0 ), length(v_b) );
+	float facror_k= smoothstep( max( radius_k - d, 0.0 ), min( radius_k + d, 1.0 ), length(v_k) );
 
-	fragColor= vec4( facror_r, facror_g, facror_b, 0.0 );
+	fragColor= vec4( facror_r * facror_k, facror_g * facror_k, facror_b * facror_k, 0.0 );
 }
