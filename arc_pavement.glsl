a175fc3dcd37347c56abe7e16a741e6c5a08499f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-23 12:03:26 +0300

    Realizovana perspektivnaja transformaçija.

--- arc_pavement.glsl
+++ arc_pavement.glsl
@@ -3,14 +3,52 @@ const int ss_factor= 3;
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-	float pix_size= 3.0 / min(iResolution.x, iResolution.y);
 	vec2 coord_shift= iTime * vec2( 0.5, 0.3 );
+
+	mat3 tilt_mat;
+	{
+		float angle= -0.14 * tau;
+		float angle_s= sin(angle);
+		float angle_c= cos(angle);
+		tilt_mat= mat3(
+			vec3(1.0, 0.0, 0.0),
+			vec3( 0.0, angle_c, angle_s),
+			vec3(0.0, -angle_s, angle_c));
+	}
+	mat3 rot_mat;
+	{
+		float angle= 0.2 * iTime;
+		float angle_s= sin(angle);
+		float angle_c= cos(angle);
+		rot_mat= mat3(
+			vec3( angle_c, angle_s, 0.0),
+			vec3(-angle_s, angle_c, 0.0),
+			vec3(0.0, 0.0, 1.0));
+	}
+	mat3 fov_mat;
+	{
+		float pix_size= 1.0 / min(iResolution.x, iResolution.y);
+		fov_mat= mat3(
+			vec3(pix_size, 0.0, 0.0),
+			vec3(0.0, pix_size, 0.0),
+			vec3(0.0, 0.0, 1.0));
+	}
+	mat3 scale_mat;
+	{
+		float scale= 3.0;
+		scale_mat= mat3( vec3(scale, 0.0, 0.0), vec3(0.0, scale, 0.0), vec3(0.0, 0.0, 1.0));
+	}
+
+	mat3 transform_mat= rot_mat * scale_mat * tilt_mat * fov_mat;
 
 	vec4 c= vec4( 0.0 );
 	for( int dx= 0; dx < ss_factor; ++dx )
 	for( int dy= 0; dy < ss_factor; ++dy )
 	{
-		vec2 coord= ( fragCoord.xy + vec2( float(dx), float(dy) ) / float(ss_factor) ) * pix_size + coord_shift;
+		vec2 ss_offset= vec2( float(dx), float(dy) ) / float(ss_factor);
+		vec3 coord_screen= vec3( ss_offset + fragCoord.xy - 0.5 * iResolution.xy, 1.0 );
+		vec3 coord_transformed= transform_mat * coord_screen;
+		vec2 coord= coord_transformed.xy / coord_transformed.z + coord_shift;
 		vec2 coord_fract= fract(coord);
 
 		vec2 center_bottom= vec2( 0.5, 0.0 );
