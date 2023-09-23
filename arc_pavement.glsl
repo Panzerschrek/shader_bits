59b8f3cd9b1949ce4f0f3b2c9fbe4037cf1dcd35
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-23 11:41:25 +0300

    Dobavlen sdvig po vremeni.

--- arc_pavement.glsl
+++ arc_pavement.glsl
@@ -4,13 +4,13 @@ const int ss_factor= 3;
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
 	float pix_size= 3.0 / min(iResolution.x, iResolution.y);
-
+	vec2 coord_shift= iTime * vec2( 0.5, 0.3 );
 
 	vec4 c= vec4( 0.0 );
 	for( int dx= 0; dx < ss_factor; ++dx )
 	for( int dy= 0; dy < ss_factor; ++dy )
 	{
-		vec2 coord= ( fragCoord.xy + vec2( float(dx), float(dy) ) / float(ss_factor) ) * pix_size ;
+		vec2 coord= ( fragCoord.xy + vec2( float(dx), float(dy) ) / float(ss_factor) ) * pix_size + coord_shift;
 		vec2 coord_fract= fract(coord);
 
 		vec2 center_bottom= vec2( 0.5, 0.0 );
