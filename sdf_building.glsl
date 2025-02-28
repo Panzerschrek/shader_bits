507e69b75100bcbb429ae9236028e4bc643500c7
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-02-28 15:54:16 +0300

    Dobavleno dviženije kamery. Realizovany tor, vycitanije figur.

--- sdf_building.glsl
+++ sdf_building.glsl
@@ -1,3 +1,7 @@
+const float g_min_marching_step= 0.002;
+const int g_max_marcging_iterations= 256;
+const float g_derivative_calculation_delta= 0.001;
+
 float sdSphere( vec3 p, float s )
 {
   return length(p)-s;
@@ -9,17 +13,29 @@ float sdRoundBox( vec3 p, vec3 b, float r )
   return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
 }
 
+float sdTorus( vec3 p, vec2 t )
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
+
 float DistanceFunction( vec3 pos )
 {
     float sphere_radius= 0.5;
     vec3 sphere_center= vec3( 0.0, 0.0, 2.0 );
     vec3 cube_center= vec3( 0.0, 0.3, 2.0 );
     vec3 cube_size= vec3( 0.6, 0.4, 0.3 );
+    vec3 torus_center= vec3( 0.5, -0.3, 2.0 );
+    vec3 torus_subtract_center= vec3( -0.5, 0.52, 2.0 );
     
     float sphere= sdSphere( pos - sphere_center, sphere_radius );
     float box= sdRoundBox( pos - cube_center, cube_size, 0.2 );
+    float torus= sdTorus( pos - torus_center, vec2( 0.5, 0.1 ) );
+    float torus_subtract= sdTorus( pos - torus_subtract_center, vec2( 0.4, 0.2 ) );
     
-    return min(sphere, box );
+    float additive_res=  min( min( sphere, box ), torus );
+    
+    return max( -torus_subtract, additive_res );
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
@@ -27,11 +43,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float pix_size= 2.2 / min(iResolution.x, iResolution.y);
     vec2 coord= ( fragCoord.xy - iResolution.xy * 0.5 ) * pix_size;
 
-    vec3 dir_normalized= normalize(vec3(coord, 1.0));
+    vec3 dir_normalized= normalize(vec3(coord, 1.5));
 
     vec3 pos= vec3(0.0, 0.0, 0.0);
+    pos.xy+= ( iResolution.xy * 0.5 - iMouse.xy ) * pix_size;
+    
     bool hit= false;
-    for( int i= 0; i < 128; ++i )
+    for( int i= 0; i < g_max_marcging_iterations; ++i )
     {
         float dist= DistanceFunction( pos );
         if( dist <= 0.0 )
@@ -39,19 +57,18 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
             hit= true;
             break;
         }
-        pos+= dir_normalized * max(0.01, dist);
+        pos+= dir_normalized * max(g_min_marching_step, dist);
     }
 
     if(hit)
     {
-        const float grad_step= 0.01;
         vec3 normal= vec3(
-            DistanceFunction( pos + grad_step * vec3(1.0, 0.0, 0.0 ) ) -
-            DistanceFunction( pos - grad_step * vec3(1.0, 0.0, 0.0 ) ),
-            DistanceFunction( pos + grad_step * vec3(0.0, 1.0, 0.0 ) ) -
-            DistanceFunction( pos - grad_step * vec3(0.0, 1.0, 0.0 ) ),
-            DistanceFunction( pos + grad_step * vec3(0.0, 0.0, 1.0 ) ) -
-            DistanceFunction( pos - grad_step * vec3(0.0, 0.0, 1.0) ) );
+            DistanceFunction( pos + g_derivative_calculation_delta * vec3(1.0, 0.0, 0.0 ) ) -
+            DistanceFunction( pos - g_derivative_calculation_delta * vec3(1.0, 0.0, 0.0 ) ),
+            DistanceFunction( pos + g_derivative_calculation_delta * vec3(0.0, 1.0, 0.0 ) ) -
+            DistanceFunction( pos - g_derivative_calculation_delta * vec3(0.0, 1.0, 0.0 ) ),
+            DistanceFunction( pos + g_derivative_calculation_delta * vec3(0.0, 0.0, 1.0 ) ) -
+            DistanceFunction( pos - g_derivative_calculation_delta * vec3(0.0, 0.0, 1.0) ) );
         normal= normalize(normal);
 
         fragColor= vec4( normal * 0.5 + vec3(0.5, 0.5, 0.5), 0.0 );
