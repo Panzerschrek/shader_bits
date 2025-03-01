718066ca6c80224004e9100fb2feb06ce1bd110f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-03-01 15:33:08 +0300

    Dobavleny vhody.

--- sdf_building.glsl
+++ sdf_building.glsl
@@ -78,13 +78,13 @@ float DistanceFunction( vec3 pos )
     vec3 pos_within_sector= vec3( cos(angle_within_sector) * sector_radius, pos.y, sin(angle_within_sector) * sector_radius );
         
     
-    const float cylinder_height= 140.0;
+    const float cylinder_height= 128.0;
     const float ground_level= -40.0;
     const float cylinder_radius= 128.0;
     const float cylinder_walls_half_thikness= 6.0;
     const float floor_cylinder_walls_half_thikness= 9.0;
     const float window0_radius= 12.0;
-    const vec3 window0_center= vec3( 0.0, 78.0, 0.0 );
+    const vec3 window0_center= vec3( 0.0, 64.0, 0.0 );
     const float window0_box_half_height= 8.0;
     const float window0_box_half_depth= cylinder_walls_half_thikness * 4.0;
     const vec3 window0_box_center= vec3( cylinder_radius, window0_center.y - window0_box_half_height, 0.0 );
@@ -100,13 +100,16 @@ float DistanceFunction( vec3 pos )
     const float window1_box_half_depth= cylinder_walls_half_thikness * 4.0;
     const vec3 window1_box_center= vec3( cylinder_radius, window1_center.y - window1_box_half_height, 0.0 );
   
-  
     const float window2_radius= 8.0;
     const vec3 window2_center= vec3( 0.0, window1_center.y - 32.0, 0.0 );
     const float window2_box_half_height= 6.0;
     const float window2_box_half_depth= cylinder_walls_half_thikness * 4.0;
     const vec3 window2_box_center= vec3( cylinder_radius, window2_center.y - window2_box_half_height, 0.0 );
   
+    const vec3 entrance_box_half_size= vec3( cylinder_walls_half_thikness * 4.0, 6.0, 3.0 );
+    const vec3 entrance_box0_center= vec3( cylinder_radius, ground_level + entrance_box_half_size.y, +5.0 );
+    const vec3 entrance_box1_center= vec3( cylinder_radius, ground_level + entrance_box_half_size.y, -5.0 );
+
     float ground_plane= sdHorizontalPlane( pos, ground_level );
     
     float window0=
@@ -124,6 +127,11 @@ float DistanceFunction( vec3 pos )
             sdXInfiniteCylinder( pos_within_sector - window2_center, window2_radius ),
             sdBox( pos_within_sector - window2_box_center, vec3( window2_box_half_depth, window2_box_half_height, window2_radius ) ) );
        
+    float entrance_box=
+        opUnion(
+            sdBox( pos_within_sector - entrance_box0_center, entrance_box_half_size ),
+            sdBox( pos_within_sector - entrance_box1_center, entrance_box_half_size ) );
+    
     float cylinder_body= sdYInfiniteHollowCylinder( pos, cylinder_radius - cylinder_walls_half_thikness, cylinder_walls_half_thikness );
     float cylinder_top= sdHorizontalPlane( pos, ground_level + cylinder_height );
     float cylinder= opIntersection( cylinder_top, cylinder_body );
@@ -139,12 +147,12 @@ float DistanceFunction( vec3 pos )
     
     float additive_res= opUnion( ground_plane, opUnion( column_cylinder, opUnion( cylinder, floor_cylinder ) ) );
     
-    return opSubtraction( additive_res, opUnion( opUnion( window0, window1 ), window2 ) );
+    return opSubtraction( additive_res, opUnion( opUnion( window0, window1 ),opUnion( window2, entrance_box ) ) );
 }
 
 mat3 CalculateRotationMatrix()
 {
-    const float tilt_angle= 0.45;
+    const float tilt_angle= 0.4;
     const float tilt_angle_cos= cos(tilt_angle);
     const float tilt_angle_sin= sin(tilt_angle);
     const mat3 tilt_mat= mat3(
